66f53fa8b8cf5df21f1b87fe5743bf1bad39ac50
Federico <federicorg07@gmail.com>
2021-08-15 11:17:59 -0300

    subiendo estructura node y express

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000")
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/productDetail.html"));
+});
+
+app.get('productCard', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/productCard"));
+});
