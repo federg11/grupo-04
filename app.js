62b2adc342592e8cfe4382013635d814a1d52f39
JuanGonzalez1889 <jgonzalez@decreditos.com>
2021-08-16 22:40:12 -0300

    agregue el logo y parte de productCard

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(3000, () => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
