5b0d2a80d6de52788280058be959d7659a7d12a6
JoackoPadin <joacopadins@gmail.com>
2021-09-01 20:58:06 -0300

    form carga prod

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 // prendemos el servidor
 app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000")
+  console.log("Servidor corriendo en puerto 3000");
 });
 
 app.use(express.static(path.join(__dirname, "public")));
-
-
